827140829caacce94c2c78ce05cff0196669c7a4
Valles Vicedo, Xavier <xavier.vallesvicedo@travelport.com>
2019-05-08 17:45:42 +0200

    Added dependency injection to resolve rulerepository to get all rules but I have problems to instanciate the IContextFactory

--- ControlTime/Program.cs
+++ ControlTime/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Travelport.AE.DataAccess.Repositories;
+using Travelport.AE.Domain.Interfaces.Repositories;
+using Travelport.AE.DataAccess.Context;
 
 namespace ControlTime
 {
     class Program
     {
+        private static IServiceProvider _serviceProvider;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RegisterServices();
+            var service = _serviceProvider.GetService<IRuleRepository>();
+            var rules = service.GetAll("xavier.vallesvicedo");
+            DisposeServices();
         }
+        private static void RegisterServices()
+        {
+            AEContext aEContext = new AEContext();
+            var collection = new ServiceCollection();
+            //collection.AddScoped<IContextFactory, ContextFactory>();
+            collection.AddScoped<IContextFactory>(s => new ContextFactory(aEContext));
+            collection.AddScoped<IRuleRepository, RuleRepository>();
+            _serviceProvider = collection.BuildServiceProvider();
+        }
+
+        private static void DisposeServices()
+        {
+            if (_serviceProvider == null)
+            {
+                return;
+            }
+            if (_serviceProvider is IDisposable)
+            {
+                ((IDisposable)_serviceProvider).Dispose();
+            }
+        }
+
     }
 }
