128775f3e68e87aee8715756b8430963500eccd8
Valles Vicedo, Xavier <xavier.vallesvicedo@travelport.com>
2019-05-08 11:55:23 +0200

    Initial commit

---
+++ ControlTime/DAO/DatabaseConnection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlTime
+{
+    class DatabaseConnection
+    {
+        public SqlConnection GetConnection()
+        {
+            SqlConnection cn = new SqlConnection(@"Server=tp-ae-develop.database.windows.net;Database=tp-ae-develop;User Id=aeadmin;Password=Travelport2017;");
+            cn.Open();
+            return cn;
+        }
+
+        //SELECT Name FROM Triggers
+        public Dictionary<int, string> GetTriggerNames()
+        {
+            var events = new Dictionary<int,string>();
+            var cont = 0;
+            SqlConnection cn = GetConnection();
+            string commandText = "SELECT Name FROM Triggers";
+            SqlCommand command = new SqlCommand(commandText, cn);
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    events.Add(cont, reader.GetString(reader.GetOrdinal("Name")));
+                    //Console.WriteLine(String.Format(cont + ": {0}", reader.GetString(reader.GetOrdinal("Name"))));
+                    cont++;
+                }
+            }
+            cn.Close();
+            return events;
+        }
+    }
+}
---
+++ ControlTime/REST/PostCall.cs
@@ -0,0 +1,47 @@
+﻿using ControlTime.Models;
+using System;
+using System.Net.Http;
+using System.Configuration;
+using System.Windows.Markup;
+namespace ControlTime
+{
+    public class PostCall
+    {
+        #region Private Methods
+        public string ProcessorCall(ProcessorRequestModel processorRequest)
+        {
+            try
+            {
+                string endpointURL = this.CreateEndpointURL(processorRequest);
+                HttpContent content = GetHttpContentFromEndpoint(endpointURL);
+                return content.ToString();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+                return ex.Message;
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private HttpContent GetHttpContentFromEndpoint(string endpointURL)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage serviceResponse = client.GetAsync(endpointURL).Result;
+            HttpContent content = serviceResponse.Content;
+            return content;
+        }
+
+        private string CreateEndpointURL(ProcessorRequestModel processorRequest)
+        {
+            return !string.IsNullOrEmpty(processorRequest.RuleId) ?
+                string.Format("{0}ruleid={1}", ConfigurationManager.AppSettings["ProcessorURL"], processorRequest.RuleId) :
+                null;
+        }
+
+        #endregion
+    }
+}
