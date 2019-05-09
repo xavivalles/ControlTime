bf6b6024804cf398fa7504f07132937096e71e20
Juanjo Montiel <juan.montiel@travelport.com>
2019-05-09 16:17:41 +0200

    Fixed context creation.

--- ControlTime/Program.cs
+++ ControlTime/Program.cs
@@ -9,67 +9,66 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ControlTime
 {
-    class Program
-    {
-        private static IServiceProvider _serviceProvider;
+	class Program
+	{
+		private static IServiceProvider _serviceProvider;
 
-        static void Main(string[] args)
-        {
-            var configuration = GetConfiguration();
-            //string connectionString = configuration["Data:DefaultConnection"];
+		static void Main(string[] args)
+		{
+			var configuration = GetConfiguration();
+			//string connectionString = configuration["Data:DefaultConnection"];
 
-            RegisterServices(configuration);
-            var service = _serviceProvider.GetService<IRuleRepository>();
+			RegisterServices(configuration);
+			var service = _serviceProvider.GetService<IRuleRepository>();
 
-            var rules = service.GetAll("xavier.vallesvicedo");
-            DisposeServices();
-        }
+			var rules = service.GetAll("xavier.vallesvicedo");
+			DisposeServices();
+		}
 
-        private static void RegisterServices(IConfiguration configuration)
-        {
-            AEContext aEContext = new AEContext();
-            var collection = new ServiceCollection();
-            collection.AddEntityFrameworkSqlServer()
-                    .AddDbContext<AEContext>(contextBuilder =>
-                    {
-                        contextBuilder.UseSqlServer(configuration["Data:DefaultConnection"]);
-                    })
-                    .AddSingleton(c => configuration);
-            //collection.AddScoped<IContextFactory, ContextFactory>();
-            collection.AddScoped<IContextFactory>(s => new ContextFactory(aEContext));
-            collection.AddScoped<IRuleRepository, RuleRepository>();
-            _serviceProvider = collection.BuildServiceProvider();
-        }
+		private static void RegisterServices(IConfiguration configuration)
+		{
+			var collection = new ServiceCollection();
+			collection.AddEntityFrameworkSqlServer()
+					.AddDbContext<AEContext>(contextBuilder =>
+					{
+						contextBuilder.UseSqlServer(configuration["Data:DefaultConnection"]);
+					})
+					.AddSingleton(c => configuration);
+			//collection.AddScoped<IContextFactory, ContextFactory>();
+			collection.AddScoped<IContextFactory, ContextFactory>();
+			collection.AddScoped<IRuleRepository, RuleRepository>();
+			_serviceProvider = collection.BuildServiceProvider();
+		}
 
-        private static void DisposeServices()
-        {
-            if (_serviceProvider == null)
-            {
-                return;
-            }
-            if (_serviceProvider is IDisposable)
-            {
-                ((IDisposable)_serviceProvider).Dispose();
-            }
-        }
+		private static void DisposeServices()
+		{
+			if (_serviceProvider == null)
+			{
+				return;
+			}
+			if (_serviceProvider is IDisposable)
+			{
+				((IDisposable)_serviceProvider).Dispose();
+			}
+		}
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    if (!optionsBuilder.IsConfigured)
-        //    {
-        //        IConfigurationRoot configuration = new ConfigurationBuilder()
-        //           .SetBasePath(Directory.GetCurrentDirectory())
-        //           .AddJsonFile("appsettings.json")
-        //           .Build();
-        //        var connectionString = configuration.GetConnectionString("DbCoreConnectionString");
-        //        optionsBuilder.UseSqlServer(connectionString);
-        //    }
-        //}
+		//protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		//{
+		//    if (!optionsBuilder.IsConfigured)
+		//    {
+		//        IConfigurationRoot configuration = new ConfigurationBuilder()
+		//           .SetBasePath(Directory.GetCurrentDirectory())
+		//           .AddJsonFile("appsettings.json")
+		//           .Build();
+		//        var connectionString = configuration.GetConnectionString("DbCoreConnectionString");
+		//        optionsBuilder.UseSqlServer(connectionString);
+		//    }
+		//}
 
-        private static IConfiguration GetConfiguration()
-        {
-            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
-            return builder.Build();
-        }
-    }
+		private static IConfiguration GetConfiguration()
+		{
+			var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+			return builder.Build();
+		}
+	}
 }
